282e29bba5c39ec88593a5992b2b34454022f2d4
Timothy097 <okeketimothy12345@gmail.com>
2023-05-24 13:21:24 +0100

    Update include new

--- stage5/UserCachingSecurity/UserSecurity.API/Models/Dtos/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserSecurity.API.Models.Dtos
-{
-    public class UserDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
